5103aa251aca928c1ff42ac1e53d2349e4917643
Suraj Yadav <surajyadav200701@gmail.com>
2024-10-18 11:03:21 +0530

    Added the Widgets ..will be working on Screens

---
+++ lib/widgets/daily_summary_card.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import '../models/weather_data.dart';
+import '../services/data_processing_service.dart';
+
+class DailySummaryCard extends StatelessWidget {
+  final WeatherData weatherData;
+  final DataProcessingService _dataService = DataProcessingService();
+
+  DailySummaryCard({required this.weatherData});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(8.0),
+      child: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(weatherData.city, style: Theme.of(context).textTheme.titleLarge),
+            SizedBox(height: 8.0),
+            Text('Temperature: ${_dataService.kelvinToCelsius(weatherData.temperature).toStringAsFixed(1)}°C'),
+            Text('Feels Like: ${_dataService.kelvinToCelsius(weatherData.feelsLike).toStringAsFixed(1)}°C'),
+            Text('Condition: ${weatherData.condition}'),
+          ],
+        ),
+      ),
+    );
+  }
+}
