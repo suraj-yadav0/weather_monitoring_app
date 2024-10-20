53b5101aa0fa6301cb595819899c8070e691dbb3
Suraj Yadav <surajyadav200701@gmail.com>
2024-10-20 16:19:33 +0530

    Added Settings PAge and Some Ui CHanges

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:weather_monitoring_app/providers/weather_providers.dart';
+import 'package:weather_monitoring_app/screens/settings_screen.dart';
 
 import 'package:weather_monitoring_app/widgets/weather_chart.dart'
     as weather_chart;
@@ -14,7 +15,14 @@ class DashboardScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Weather Dashboard'),
+        title: const Text('Weather Dashboard'),centerTitle: true,actions: [
+          IconButton(
+            icon: const Icon(Icons.settings),
+            onPressed: () => Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) => const SettingsScreen(),
+            )), 
+          ),
+        ],
       ),
       body: Consumer<WeatherProvider>(
         builder: (context, weatherProvider, child) {
